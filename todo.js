b4e47e327fbe85c7a8e9cd5e89e208b7d4410174
gydms741 <gydms741@naver.com>
2021-04-19 01:12:07 +0900

    add todo

---
+++ todo.js
@@ -0,0 +1,53 @@
+const toDoForm =  document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.value = "❌";
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    span.innerHTML = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos(){
+    const loadedToDos =  lacalStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            console.log(toDo.text);
+        });
+    }
+}
+
+function init(){
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
